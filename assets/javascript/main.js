7075c0dcee58ea07d45893f0f71eb444cb899ad0
Aidan Dai <aidandai@126.com>
2016-03-10 13:13:17 +0800

    work something,if you want to details, please view it on github and code!

---
+++ assets/javascript/main.js
@@ -0,0 +1,25 @@
+/**
+ * 获取DOM节点元素
+ * @param  object obj exp:{id:"menu"}
+ * @return array exp:ul
+ */
+function getE(obj){
+	return elements;
+}
+/**
+ * 添加class属性
+ * @param object element DOM节点元素
+ * @return boolean true/flase
+ */
+function addClass(element){
+	return result;
+}
+/**
+ * 判断是否有class
+ * @param object element DOM节点元素
+ * @param string strClass class
+ * @return Boolean [description]
+ */
+function hasClass(element, strClass){
+	return result;
+}
